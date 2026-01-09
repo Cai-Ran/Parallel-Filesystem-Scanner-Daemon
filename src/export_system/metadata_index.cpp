06a21a7e00a78de1cb1e81a42ed681e76cce71dc
Cai-Ran <cai.work.career@gmail.com>
2026-01-09 20:37:15 +0800

    feat(metadata_index): implement update and get with mutex-protected map access

---
+++ src/export_system/metadata_index.cpp
@@ -0,0 +1,31 @@
+#include <metadata_index.h>
+#include <async_logger.h>
+#include <cassert>
+
+
+void 
+MetadataIndex::update(std::string&& path, Entry&& ent) {
+    std::lock_guard<std::mutex> lock(mtx);
+    std::unordered_map<std::string, Entry>::iterator it = index.find(path);
+    if (it == index.end()) {
+        index.emplace(std::move(path), std::move(ent));
+        version_cnt++;
+    }
+    else if (it->second.fp != ent.fp) {
+        it->second = std::move(ent);
+        version_cnt++;
+    }
+}
+
+bool 
+MetadataIndex::get(const std::string& path, Entry& result) const {
+    std::lock_guard<std::mutex> lock(mtx);
+    std::unordered_map<std::string, Entry>::const_iterator it = index.find(path);       //const function
+
+    if (it == index.end()) {
+        return false;
+    }
+
+    result = it->second;
+    return true;
+}
