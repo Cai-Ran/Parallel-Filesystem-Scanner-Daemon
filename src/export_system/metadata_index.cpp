206411e31d7443256f7091140df101f7cc32deff
Cai-Ran <cai.work.career@gmail.com>
2026-01-10 19:24:31 +0800

    feat(metadata_index): implement erase contains index_size and version query methods

--- src/export_system/metadata_index.cpp
+++ src/export_system/metadata_index.cpp
@@ -29,3 +29,42 @@ MetadataIndex::get(const std::string& path, Entry& result) const {
     result = it->second;
     return true;
 }
+
+
+bool
+MetadataIndex::erase(const std::string& path) {
+    std::lock_guard<std::mutex> lock(mtx);
+    std::unordered_map<std::string, Entry>::const_iterator it = index.find(path);
+    if (it == index.end()) {
+        AsyncLogger::logger().error("path not in index, cannot erase");
+        return false;
+    }
+    index.erase(it);
+    version_cnt++;
+    return true;
+}
+
+
+bool 
+MetadataIndex::contains(const std::string& path) const {
+    std::lock_guard<std::mutex> lock(mtx);
+    std::unordered_map<std::string, Entry>::const_iterator it = index.find(path);
+    if (it == index.end()) return false;
+    // if (it->second.state == Entry::EntryState::Alive) return true;
+    return true;
+};
+
+size_t 
+MetadataIndex::index_size() const {
+    std::lock_guard<std::mutex> lock(mtx);
+    return index.size();
+}
+
+uint64_t
+MetadataIndex::get_version_number() const {
+    std::lock_guard<std::mutex> lock(mtx);
+    return version_cnt;
+}
+
+
+
