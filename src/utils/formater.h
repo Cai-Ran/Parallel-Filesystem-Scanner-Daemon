2e25c67a5894989daa62a864754796fb70d6fe40
Cai-Ran <cai.work.career@gmail.com>
2026-01-28 18:16:43 +0800

    feat(formater): add JsonContent model and validate/transform helpers

---
+++ src/utils/formater.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <entry.h>
+#include <string>
+
+
+enum class EventState: uint8_t{
+    Alive,
+    Error,
+    Deleted,
+    Canceled
+};
+
+struct JsonContent {
+    std::string path;
+    NodeType node_type;
+    EventState state;
+    std::string err_msg;
+    uint64_t size;
+    std::string time;
+};
+
+
+namespace formater {
+
+    bool validate_outdir(const std::string& path);
+    bool validate_outpath(const std::string& path);
+
+    void transform_entry(Entry&& e, std::string&& path, JsonContent& json, EventState state);
+    void transform_event(FileEvent&& e, JsonContent& json, EventState state);
+
+    std::string format_node(NodeType node_type);
+    std::string format_state(EventState state);
+
+    std::string format_time(const time_t t);
+
+};
+
