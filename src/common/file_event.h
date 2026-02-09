3749b852e59f95c657be3c176d51a2b471819278
Cai-Ran <cai.work.career@gmail.com>
2026-02-09 20:41:12 +0800

    feat(common): add FileEvent and NodeType definitions for scanner output

---
+++ src/common/file_event.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <system_error>
+#include <cstdint>
+
+enum class NodeType: uint8_t {
+    UNKNOWN,
+    DIR,
+    FILE,
+    LINK
+};
+
+
+struct FileEvent {
+    std::string path;
+
+    NodeType node_type;
+
+    time_t  modtime;               
+    uint64_t size;
+
+    std::error_code err;            //permission denied/broken path/
+};
+
+
