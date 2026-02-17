0169768ddeef60eee699d013d5166d1dee4f50bc
Cai-Ran <cai.work.career@gmail.com>
2026-02-17 20:44:12 +0800

    feat(config): add Config singleton switch clamp and accessor api

--- src/utils/config.h
+++ src/utils/config.h
@@ -43,3 +43,57 @@ struct ExportManagerConfig {
 
 
 
+class Config {
+    private:
+        static Config* config_ptr;
+
+    public:
+        Config() {};
+        static void switch_config(Config& ins) {
+            config_ptr = &ins;
+        }
+        static Config& cfg() {
+            static Config global_cfg;        //HERE create global config
+            if (!config_ptr)    config_ptr = &global_cfg;
+            return *config_ptr;
+        };
+
+    private:
+        // copy forbidden
+        Config(const Config&) = delete;                     //Config b = a;
+        Config& operator=(const Config& other) = delete;    // b = a;
+        // move forbidden
+        Config(Config&&) = delete;                    //Config b = std::move(a);  
+        Config& operator=(Config&& other) = delete;   //b = std::move(a);
+
+        DaemonConfig        daemon_;
+        HttpServerConfig    httpserver_;
+        SchedulerConfig     scheduler_;
+        AsyncLoggerConfig   asynclogger_;
+        ExportManagerConfig exportmanager_;
+        ManagerConfig       manager_;
+
+        template <typename F, typename T>
+        T clamp(F old, T new_min, T new_max) {
+            if (old < static_cast<F>(new_min))   return new_min;
+            if (old > static_cast<F>(new_max))   return new_max;
+            return static_cast<T>(old);
+        }
+
+    public:
+        bool load(const std::string& file_path);
+
+        const DaemonConfig&
+            daemon() const {        return daemon_; }
+        const HttpServerConfig& 
+            httpserver() const {    return httpserver_; }
+        const SchedulerConfig& 
+            scheduler() const {     return scheduler_; }
+        const AsyncLoggerConfig&
+            asynclogger() const {    return asynclogger_; }
+        const ExportManagerConfig&
+            exportmanager() const { return exportmanager_; }
+        const ManagerConfig&
+            manager() const { return manager_; }
+};
+
