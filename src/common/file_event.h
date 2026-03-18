41bf521c848fdcf1dbb9d89109c86a3c84c1148f
Cai-Ran <cai.work.career@gmail.com>
2026-03-18 19:58:22 +0800

    chore: update src/common/file_event.h

--- src/common/file_event.h
+++ src/common/file_event.h
@@ -11,15 +11,42 @@ enum class NodeType: uint8_t {
 };
 
 
+struct Sentinel {
+    bool is_sentinel = false;
+    bool is_canceled = false;
+    Sentinel():is_sentinel(false), is_canceled(false){};
+};
+
+
+//order format to match padding
 struct FileEvent {
+    uint64_t scan_id;
+
+    time_t   modtime;
+    uint64_t size;
+
+    std::error_code err;            //permission denied/broken path/
+
     std::string path;
 
     NodeType node_type;
 
-    time_t  modtime;               
-    uint64_t size;
-
-    std::error_code err;            //permission denied/broken path/
+    
+    Sentinel end_signal;       
+    //marks end of a scan; 
+    //path = root_path
+    //scan_id = id
+    //others unused
 };
+
+
+
+enum class EventState: uint8_t{
+    Alive,
+    Error,
+    Deleted,
+    Canceled
+};
+
 
 
