329a6c96cb14d0ea0cd46aca22febfbe5dcf3596
Cai-Ran <cai.work.career@gmail.com>
2026-03-29 20:14:45 +0800

    chore: update src/scan_system/manager.h

--- src/scan_system/manager.h
+++ src/scan_system/manager.h
@@ -1,10 +1,8 @@
 #pragma once
 
-#include <metadata_index.h>
 #include <multi_scanner.h>
 #include <export_manager.h>
-#include <scan_task.h>
-#include <scan_data.h>
+#include <scan_types.h>
 #include <job_queue.h>
 #include <thread_pool.h>
 #include <config.h>
@@ -32,8 +30,6 @@ private:
     ExportManager export_manager;
     ThreadPool<ScanData> scan_pool;
 
-    std::thread export_manager_thread;
-
     //scheduler callback
     std::function<void(uint64_t scan_id)> notify_scan_finished;
     std::function<void()> notify_dispatch_available;
@@ -43,7 +39,6 @@ private:
     std::mutex registry_mtx;
     std::condition_variable manager_cv;
 
-    bool transfer_result(uint64_t scan_id, ExportManager::ExportData& data);
 
 
 public:
@@ -59,21 +54,18 @@ public:
     // daemon api
     void start();
     void set_scheduler_callback(std::function<void(uint64_t scan_id)> fn1, 
-                                std::function<void()> fn2);    
+                                std::function<void()> fn2,
+                                std::function<void(const std::string& root)> fn3);    
     bool clean_up(uint64_t scan_id);
 
-    bool set_export_dir(std::string&& export_dir);
+ 
     // bool check_exported(uint64_t scan_id);
     std::vector<bool> check_exported(const std::vector<uint64_t>& scan_ids);
-    bool export_report(uint64_t scan_id,\
-                        std::string& result_path, std::string& summary_path);
-    bool get_newest_index(uint64_t& version_number, time_t& snapshot_timestamp);
-    bool index_report(uint64_t scan_id, \
-                        std::string& detail_path, std::string& summary_path);
 
     // scanner api
     JobQueue<ScanData>::SubmitResult task_on_job_submit(ScanData& data, bool is_root);
     void task_on_job_finish(uint64_t scan_id, std::shared_ptr<ScanContext> ctx);
+    void record_result(FileEvent&& event);
 
 };
 
